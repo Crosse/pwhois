baa259bd1aca66a1fc5da9fedc1b039010715564
Seth Wright <seth@crosse.org>
2017-12-19 12:14:37 -0500

    bump patch version for license change

--- lib/pwhois/version.rb
+++ lib/pwhois/version.rb
@@ -2,7 +2,7 @@ module Pwhois
     module Version
         MAJOR = 1
         MINOR = 2
-        TINY  = 1
+        TINY  = 2
     end
     VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
 end
